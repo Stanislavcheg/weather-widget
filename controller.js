ab0bbb7dc77527430ceefdc473d62e6b55f34090
Stanislavcheg <stanislavcheg@mail.ru>
2016-11-07 21:35:04 +0200

    initial commit

---
+++ controller.js
@@ -0,0 +1,77 @@
+
+
+
+var weather = angular.module('weather', ['ngResource']);
+
+weather.controller('weatherCtrl', function weatherCtrl($scope) {
+		$scope.date = function(daysForward){
+			var date = new Date();
+			if (daysForward) {
+			date.setDate(date.getDate() + daysForward);
+			}
+			return date;
+		};
+
+		$scope.weatherIcons = {
+		"Rain" : "images/icons/icon-9.svg",
+		"Clouds" : "images/icons/icon-3.svg",
+		"Atmosphere" : "images/icons/icon-7.svg",
+		"Thunderstorm" : "images/icons/icon-12.svg",
+		"Drizzle" : "images/icons/icon-5.svg",
+		"Snow" : "images/icons/icon-14.svg",
+		"Clear" : "images/icons/icon-2.svg",
+		"Extreme" : "images/icons/icon-8.svg",
+		"Additional" : "images/icons/icon-3.svg"
+		};
+
+	$scope.search = function ($event) {
+		if ($event) $event.preventDefault();
+		$scope.input = $scope.city;
+	}
+});
+
+
+weather.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
+
+weather.directive('widget', function (weatherResource, weatherForecastResource) {
+	return {
+		restrict: 'E',
+		link: function (scope, element) {
+			scope.$watch('input', function (n) {
+				if (n) {
+					weatherResource.get({city: n}, function (response) {
+						scope.data = response;
+						weatherForecastResource.get({city: n, country:  scope.data.sys.country}, function (response) {
+						scope.dataForc = response;
+					});
+					});
+					
+				}
+			});
+		}
+	}
+});
+
+weather.factory('weatherResource', function ($resource) {
+	var api_key = '6e3fcb812a6ea26bfbb60bacee7afa6f',
+		path = 'http://api.openweathermap.org/data/2.5/weather?q=:city&units=metric&appid=' + api_key;
+	return $resource(path);
+});
+
+weather.factory('weatherForecastResource', function ($resource) {
+	var api_key = '6e3fcb812a6ea26bfbb60bacee7afa6f',
+		path = 'http://api.openweathermap.org/data/2.5/forecast?q=:city,country&units=metric&appid=' + api_key;
+	return $resource(path);
+});
